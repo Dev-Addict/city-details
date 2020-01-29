0ba979797e26b7b15dece09f5bbaa079f7675eb0
AriaMAN-ACT <aria.azadi.pour-act@protonmail.com>
2020-01-30 01:21:53 +0330

    Updates

---
+++ precache-manifest.b84f18800c68777b317e9406e13a0270.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ec2044286fc17e8c8cc8f6e56f07f54",
+    "url": "/city-details/index.html"
+  },
+  {
+    "revision": "0792b7a4d18ccdc2116f",
+    "url": "/city-details/static/css/main.935d03ba.chunk.css"
+  },
+  {
+    "revision": "885a8262fec1aa7e0ad7",
+    "url": "/city-details/static/js/2.318786be.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/city-details/static/js/2.318786be.chunk.js.LICENSE"
+  },
+  {
+    "revision": "0792b7a4d18ccdc2116f",
+    "url": "/city-details/static/js/main.e4d71bc6.chunk.js"
+  },
+  {
+    "revision": "1dcb8ea244ab8739217d",
+    "url": "/city-details/static/js/runtime-main.fda72fad.js"
+  }
+]);
